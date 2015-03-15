44a99ddceebb036278b76eeec11e5699e18a3078
Ben Shryock <benshry@gmail.com>
2015-03-15 02:53:22 -0400

    python script to remove identifying information from grades csv

---
+++ grades.py
@@ -0,0 +1,29 @@
+import csv
+import json
+
+col = {
+    'ext':2,
+    'ps1':5,
+    'ps2':9,
+    'ps3':13,
+    'ps4':17
+}
+
+
+if __name__ == "__main__":
+
+    data = []
+
+    reader = csv.reader(open('files/grades.csv','rU'))
+    for row in reader:
+        data_object = {
+            'ext': row[col['ext']],
+            'ps1': row[col['ps1']],
+            'ps2': row[col['ps2']],
+            'ps3': row[col['ps3']],
+            # 'ps4': row[col['ps4']],
+        }
+        data.append(data_object)
+
+    with open('output/grades.json', 'w') as out_file:
+        json.dump(data, out_file)
