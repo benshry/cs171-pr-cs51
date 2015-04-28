44fc8667226877dca33861624225fc4c61cc77e4
Ben Shryock <benshry@gmail.com>
2015-04-28 05:51:53 -0400

    moved helper functions to more general file

--- js/comfortvis.js
+++ js/comfortvis.js
@@ -170,38 +170,3 @@ ComfortVis.prototype.onSelectionChange = function (id, min, max) {
     this.updateVis();
 }
 
-/* todo: move to different file */
-var hasOwnProperty = Object.prototype.hasOwnProperty;
-
-function isEmpty(obj) {
-
-    // null and undefined are "empty"
-    if (obj == null) return true;
-
-    // Assume if it has a length property with a non-zero value
-    // that that property is correct.
-    if (obj.length > 0)    return false;
-    if (obj.length === 0)  return true;
-
-    // Otherwise, does it have any properties of its own?
-    // Note that this doesn't handle
-    // toString and valueOf enumeration bugs in IE < 9
-    for (var key in obj) {
-        if (hasOwnProperty.call(obj, key)) return false;
-    }
-
-    return true;
-}
-
-function multiFilter(data, filterFunctions) {
-  filtered = [];
-  for (item in data) {
-    for (key in filterFunctions) {
-      if (filterFunctions[key](data[item])) {
-        filtered.push(data[item]);
-        break;
-      }
-    }
-  }
-  return filtered;
-}
