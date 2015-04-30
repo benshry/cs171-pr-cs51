b407948069a9b0f0c4d94e6da717364afc3094d1
Ben Shryock <benshry@gmail.com>
2015-04-29 23:17:54 -0400

    piazza updates on comfort click

--- js/piazzavis.js
+++ js/piazzavis.js
@@ -180,3 +180,16 @@ PiazzaVis.prototype.onSelectionChange = function (id, min, max) {
 
     this.updateVis();
 }
+
+PiazzaVis.prototype.onComfortChange = function (id, comfort) {
+
+    var that = this;
+
+    var filter = function(d) {
+      return d.comfort == comfort;
+    }
+
+    this.wrangleData(filter, id);
+
+    this.updateVis();
+}
